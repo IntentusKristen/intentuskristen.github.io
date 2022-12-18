89a96a051bf4159d5dc2cd3beee385d0c218704c
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2022-12-17 22:41:26 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Banner/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
