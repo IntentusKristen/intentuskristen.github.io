818939e5327863424cd3e925ee808f56a6b3773a
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2022-12-22 17:06:27 -0500

    fixed routing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { Skills } from './components/Skills';
 import { ProjectComp } from './components/ProjectsComp';
 import { Contact } from './components/Contact';
