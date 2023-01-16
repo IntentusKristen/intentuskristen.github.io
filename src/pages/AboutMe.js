63b100ba8371d22bf9b3d7f162a4dd48a0426f85
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2023-01-16 11:22:30 -0500

    fixed aboutme

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -16,16 +16,12 @@ const AboutMe = () => {
         <NavBarAbout/>
         <section className="aboutme">
             <Container className="page-pad" style = {padStyle}>
-                <Row className="align-items-top">
-                    <Col md={6} className="d-flex justify-content-center">
-                        <h1>About Me</h1>
-                    </Col>
-                </Row>
                 <Row>
                     <Col md={6} className="d-flex justify-content-center">
                         <img className="headshot" src={headshot} alt="Headshot"/>
                     </Col>
                     <Col md={6}>
+                    <h1>Learn More About Me</h1>
                         <div className="d-flex justify-content-center">
                             <p>Hi and welcome to my website! My name is Kristen Zhang and I am currently in my third year at Western University, pursuing a dual degree in software engineering and business. 
                             I have always loved solving problems since I was young and fell in love with coding in university. My main coding languages are Python, JavaScript, and C, and I have been able to apply these skills in various projects -- you can check them out in the "Projects" tab!</p>
