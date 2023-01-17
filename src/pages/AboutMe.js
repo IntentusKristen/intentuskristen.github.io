a11d5c89c2ca301a23648bbc65c3a6744672eae2
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2023-01-17 15:25:42 -0500

    Add: Resume link to AboutMe page

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,29 +1,33 @@
 import React from 'react'
-import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import headshot from "../assets/img/headshot.jpg";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import resume from "../assets/img/resume.pdf"
 
 const AboutMe = () => {
     // creating inline styling with react
     const padStyle = 
             {
-                paddingTop: "200px",
+                paddingTop: "250px",
             }
 
   return (
     <div className="App">
         <section className="aboutme">
-            <Container className="page-pad" style = {padStyle}>
+            <Container style={padStyle}>
                 <Row>
                     <Col md={6} className="d-flex justify-content-center">
                         <img className="headshot" src={headshot} alt="Headshot"/>
                     </Col>
                     <Col md={6}>
                         <div className="d-flex justify-content-left" style={{textAlign: 'left',}}>
-                            <p>👋🏼 - Hi I'm Kristen! <br></br> 
-                            📚 - I am currently in my third year at Western University, pursuing a dual degree in software engineering and business. <br></br>
-                            💻  - My main coding languages are Python, JavaScript, and C, and I have been able to apply these skills in various projects -- you can check them out in the "Projects" tab!</p>
+                            <p>👋🏼 - Hi I'm Kristen! <br></br> <br></br> 
+                            📚 - I am currently in my third year at Western University, pursuing a dual degree in software engineering and business. <br></br> <br></br> 
+                            👩🏻‍💻  - My main coding languages are Python, JavaScript, and C, and I have been able to apply these skills in various projects -- you can check them out in the "Projects" tab! <br></br>
+                            </p>
+                            
                         </div>
+                        <button onClick={() => window.open(resume,"_blank")}>Check out my resume here<ArrowRightCircle size={25}/></button>
                     </Col>
                 </Row>
             </Container>
