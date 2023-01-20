5cd8d88aa12d4283c1beb799f8465daa6d7643d8
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2023-01-19 22:17:23 -0500

    feat: added styling to contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -16,15 +16,51 @@ const Contact = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className='App'>
+    <section className="contact" id="connect">
+        <Container className="page-pad">
+            <Row className="align-items-center">
+                <Col md={6} className="d-flex justify-content-center">
+                    <img className="contact-img" src={contactImg} alt="Contact Us"/>
+                </Col>
+                <Col md={6}>
+                    <div className="d-flex justify-content-center">
+                        <h2>Send Me a Message!</h2>
+                    </div>
+                    
+                    <form onSubmit={sendForm}>
+                        <Row>
+                            <Col sm={6} className="px-1 d-flex justify-content-end">
+                                <input className="input-field" type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e)=> onFormUpdate('firstName', e.target.value)}/>
+                            </Col>
+                            <Col sm={6} className="px-1 d-flex justify-content-start">
+                                <input className="input-field" type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e)=> onFormUpdate('lastName', e.target.value)}/>
+                            </Col>
+                            <Col sm={6} className="px-1 d-flex justify-content-end">
+                                <input className="input-field" type="email" value={formDetails.email} placeholder="Email" onChange={(e)=> onFormUpdate('email', e.target.value)}/>
+                            </Col>
+                            <Col sm={6} className="px-1 d-flex justify-content-start">
+                                <input className="input-field" type="tel" value={formDetails.phone} placeholder="Phone" onChange={(e)=> onFormUpdate('phone', e.target.value)}/>
+                            </Col>
+                            <Col sm={12} className="d-flex justify-content-center">
+                                <textarea className="message-box" row="6" value={formDetails.message} placeholder="Message" onChange={(e)=> onFormUpdate('message', e.target.value)}/>
+                            </Col>
+                            <Col className="d-flex justify-content-center">
+                                <button className="contact-button" type="submit"><span>{buttonText}</span></button>
+                            </Col>
+                            {
+                                status.message &&
+                                <Col>
+                                    <p className={status.success === false ? "danger": "success"}></p>
+                                </Col>
+                            }
+                        </Row>
+                    </form>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+    </div>
   );
 };
 
