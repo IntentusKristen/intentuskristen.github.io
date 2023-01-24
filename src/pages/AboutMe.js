32588e9729888ec2ed93ff612d3f8365ff3738ae
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2023-01-24 14:50:26 -0500

    style: changed font sizing

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -8,26 +8,30 @@ const AboutMe = () => {
     // creating inline styling with react
     const padStyle = 
             {
-                paddingTop: "250px",
+                paddingTop: "200px",
             }
 
   return (
     <div className="App">
         <section className="aboutme">
             <Container style={padStyle}>
-                <Row>
-                    <Col md={6} className="d-flex justify-content-center">
+                <Row className="d-flex justify-content-center">
+                <Col md={1}></Col>
+                    <Col md={5} className="d-flex justify-content-center">
                         <img className="headshot" src={headshot} alt="Headshot"/>
                     </Col>
-                    <Col md={6}>
-                        <div className="d-flex justify-content-left" style={{textAlign: 'left',}}>
-                            <p>👋🏼 - Hi I'm Kristen! <br></br> <br></br> 
-                            📚 - I am currently in my third year at Western University, pursuing a dual degree in software engineering and business. <br></br> <br></br> 
+                    <Col md={5}>
+                    <h1 style={{textAlign: 'left',}}><b>Hello</b></h1>
+                        <div className="d-flex justify-content-left" style={{textAlign: 'left'}}>
+                            
+                            <p>👋🏼 - I'm Kristen! <br></br>
+                            📚 - I am currently in my third year at Western University, pursuing a dual degree in software engineering and business.<br></br> 
                             👩🏻‍💻  - My main coding languages are Python and JavaScript.</p>
                             
                         </div>
                         <button onClick={() => window.open(resume,"_blank")}>Check out my resume here<ArrowRightCircle size={25}/></button>
                     </Col>
+                    <Col md={1}></Col>
                 </Row>
             </Container>
         </section>
