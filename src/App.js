3e64046926e1fc4e8231ab457d0f16c3f5de1d9d
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2023-01-25 23:13:38 -0500

    Revert "Updates"
    This reverts commit 90eeab4981c6b16e9466d75858b4cd6b17d1b77e.

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from './pages/Contact';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import AboutMe from './pages/AboutMe';
+import NavBar from './components/NavBar';
+import React from 'react';
+import Experience from './pages/Experience';
+import { Footer } from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/aboutme" element={<AboutMe/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/experience" element={<Experience/>}/>
+      </Routes>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
