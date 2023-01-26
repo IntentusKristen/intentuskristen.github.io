6fd9a982ce6cbf99ff4ec9def6919dfa5e455d1f
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2023-01-26 13:39:23 -0500

    perf: routing doesn't flash for most pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Contact from './pages/Contact';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { HashRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import AboutMe from './pages/AboutMe';
 import NavBar from './components/NavBar';
@@ -13,12 +13,12 @@ function App() {
   return (
     <>
       <NavBar/>
-      <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/aboutme" element={<AboutMe/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/experience" element={<Experience/>}/>
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/aboutme" element={<AboutMe/>}/>
+          <Route exact path="/contact" element={<Contact/>}/>
+          <Route exact path="/experience" element={<Experience/>}/>
+          </Routes>
       <Footer/>
     </>
   );
