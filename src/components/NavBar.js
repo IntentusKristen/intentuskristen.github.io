8b29574106707314226750d8adbd39a6e7294ca9
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2023-01-26 23:20:45 -0500

    Revert "Updates"
    This reverts commit 19161311d0a000b4036b04d01ca472ee715de142.

---
+++ src/components/NavBar.js
@@ -0,0 +1,64 @@
+// need to import functionalities
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { useState, useEffect, useDeferredValue } from "react";
+import logo from '../assets/img/logo.png';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+
+// () basically placeholder for function()
+const NavBar = () => {
+    const [activeLink, setActiveLink ]= useState('/');
+    // detect whether user scrolled or not
+    const [scrolled, setScrolled] = useState('false')
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled('true');
+            } else {
+                setScrolled('false');
+            }
+        }
+
+        window.addEventListener("scroll", onScroll);
+
+        // remove event listeners when you don't plan on using them anymore
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+
+    const onUpdateActiveLink = (value) => {
+      setActiveLink(value);
+    }
+
+    return (
+        <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
+        <Container style={{paddingLeft: '0', paddingRight: '0'}}>
+          <Navbar.Brand href="/">
+            <img class='resize' src={logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/" exact className={activeLink === '/' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('/')}>Home</Nav.Link>
+              <Nav.Link href="/#/aboutme" exact className={activeLink === '/#/aboutme' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('/#/aboutme')}>About Me</Nav.Link>
+              <Nav.Link href="/#/experience" exact className={activeLink === '/#/experience' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('/#/experience')}>Experience</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                  <a href="https://www.facebook.com/kristen.zhang.9655/" target="_blank" ><span style={{width: '59.25%'}}><FacebookIcon sx={{color: 'white'}}/></span></a>
+                  <a href="https://www.linkedin.com/in/kristen-zhang/" target="_blank"><span><LinkedInIcon sx={{color: 'white'}}/></span></a>
+                  <a href="https://github.com/IntentusKristen" target="_blank"><span style={{width: '59.25%'}}><GitHubIcon sx={{color: 'white'}}/></span></a>
+                  <button className="vvd" onClick={() => window.location.href="/#/contact"}><span>Let's Connect</span> </button>
+              </div>
+                
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+}
+
+export default NavBar;
